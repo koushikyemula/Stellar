d528c0e837957933a08c81f425b6fbf5cb61ff9d
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-08-11 17:48:09 +0530

    setting up folder , routes and clerk auth along with signin and signout pages

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <p> dashboard page (protected)</p>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
+}
+
+export default DashboardPage;
