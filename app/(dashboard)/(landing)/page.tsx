6e922e7bf490c0540637a6436970b13ef2c316a8
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-08-22 17:53:12 +0530

    landing hero component

--- app/(dashboard)/(landing)/page.tsx
+++ app/(dashboard)/(landing)/page.tsx
@@ -1,9 +1,11 @@
+import { LandingHero } from "@/components/landing-hero";
 import { LandingNavbar } from "@/components/landing-navbar";
 
 const LandingPage = () => {
     return ( 
         <div className="h-full">
             <LandingNavbar />
+            <LandingHero />
         </div>
      );
 }
