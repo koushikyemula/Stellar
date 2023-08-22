065c35b2f27dabeacdd18b5ec8e95b0ebfd9aa8e
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-08-22 17:23:37 +0530

    landing navbar and landing page p-1

--- app/(dashboard)/(landing)/page.tsx
+++ app/(dashboard)/(landing)/page.tsx
@@ -1,23 +1,9 @@
-import Link from "next/link";
-
-import { Button } from "@/components/ui/button";
+import { LandingNavbar } from "@/components/landing-navbar";
 
 const LandingPage = () => {
     return ( 
-        <div>
-             landing page (unprotected)
-            <div>
-                <Link href="/sign-in">
-                <Button >
-                    Login
-                </Button>
-                </Link>
-                <Link href="/sign-up">
-                <Button >
-                    Register
-                </Button>
-                </Link>
-            </div>
+        <div className="h-full">
+            <LandingNavbar />
         </div>
      );
 }
