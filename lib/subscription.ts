4450ec6988fad7c841bc732991e1c5c446fcbfa7
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-09-14 10:26:05 +0530

    lib code format and svg fix

--- lib/subscription.ts
+++ lib/subscription.ts
@@ -5,29 +5,32 @@ import prismadb from "@/lib/prismadb";
 const DAY_IN_MS = 86_400_000;
 
 export const checkSubscription = async () => {
-  const { userId } = auth();
+	const { userId } = auth();
 
-  if(!userId) {
-    return false;
-  }
+	if (!userId) {
+		return false;
+	}
 
-  const userSubscription = await prismadb.userSubscription.findUnique({
-    where: {
-      userId: userId,
-    },
-    select: {
-      stripeSubscriptionId: true,
-      stripeCurrentPeriodEnd: true,
-      stripeCustomerId: true,
-      stripePriceId:true,
-    },
-  });
+	const userSubscription = await prismadb.userSubscription.findUnique({
+		where: {
+			userId: userId,
+		},
+		select: {
+			stripeSubscriptionId: true,
+			stripeCurrentPeriodEnd: true,
+			stripeCustomerId: true,
+			stripePriceId: true,
+		},
+	});
 
-  if(!userSubscription) {
-    return false;
-  }
+	if (!userSubscription) {
+		return false;
+	}
 
-  const isValid = userSubscription.stripePriceId && userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
+	const isValid =
+		userSubscription.stripePriceId &&
+		userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS >
+			Date.now();
 
-  return !!isValid;
-}
+	return !!isValid;
+};
