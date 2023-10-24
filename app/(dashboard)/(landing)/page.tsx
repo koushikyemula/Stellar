87ca36029df68606457da23561fb793013a8d970
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-24 17:53:58 +0530

    preittier fixes

--- app/(dashboard)/(landing)/page.tsx
+++ app/(dashboard)/(landing)/page.tsx
@@ -3,13 +3,13 @@ import { LandingHero } from "@/components/landing-hero";
 import { LandingNavbar } from "@/components/landing-navbar";
 
 const LandingPage = () => {
-	return (
-		<div className="h-full">
-			<LandingNavbar />
-			<LandingHero />
-			<LandingContent />
-		</div>
-	);
+  return (
+    <div className="h-full">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+    </div>
+  );
 };
 
 export default LandingPage;
